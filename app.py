d0b0bd6cd770fd644ea22aaa5f057536e0c4fd4c
harloxx <chloe0105@naver.com>
2021-11-24 15:23:15 +0900

    PoC done

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, flash, redirect, render_template, request, url_for, send_from_directory, session
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+currentPath=os.getcwd()
+app.config['UPLOAD_FOLDER'] = currentPath+'\\uploads'
+
+@app.route('/upload')
+def uploadfile():
+    return render_template('uupload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader_file():
+    if request.method=='POST':
+        upload = request.files.getlist("file[]")
+        print("hi")
+        for f in upload:
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        return 'file uploaded successfully'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
