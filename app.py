dce99f4152b9f1b0b1e075628786dadba4775a81
harloxx <chloe0105@naver.com>
2021-12-05 16:22:04 +0900

    add malicious benign function + return result.html

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ app.secret_key = "super secret key"
 currentPath=os.getcwd()
 app.config['UPLOAD_FOLDER'] = currentPath+'\\templates\\uploads'
 
+def output():
+    list_file = open('a.txt', 'r').read().split('\n')
+    malicious_list=[]
+    benign_list=[]
+    for file in list_file:
+        if (file.split(' ')[2]=="M"):
+            malicious_list.append(file.split(' ')[0])
+        else:
+            benign_list.append(file.split(' ')[0])
+    return(malicious_list,benign_list)
+
+
+@app.route('/',methods=['GET'])
+def a():
+    if request.method=="GET":
+        value=1
+        return render_template('jsonTest.html',value=value)
 
 
 @app.route('/upload')
@@ -19,11 +36,14 @@ def uploader_file():
         upload = request.files.getlist("file[]")
         for f in upload:
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-        test=True
-        if (test==False):
-            return render_template('success.html')
-        else:
-            return render_template('success.html')
+        malicious,benign=output()
+        return render_template('result.html',benign=benign,malicious=malicious)
+        #test=True
+        #if (test==False):
+        #    return render_template('success.html')
+        #else:
+        #    return render_template('result.html',benign=benign,malicious=malicious)
+            #return render_template('jsonTest.html',malicious=malicious)
         #return 'file uploaded successfully'
         #return render_template('malicious.html')
 
