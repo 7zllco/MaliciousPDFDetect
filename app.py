582bc0a30a1a822ef72f4b5773ba8879849e738d
harloxx <chloe0105@naver.com>
2021-12-05 18:55:22 +0900

    ubuntu change

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app.secret_key = "super secret key"
 currentPath=os.getcwd()
 
 #pdf의 파일을 받아온 뒤, 업로드 되는 경로 설정
-app.config['UPLOAD_FOLDER'] = currentPath+'\\templates\\uploads'
+app.config['UPLOAD_FOLDER'] = currentPath+'/templates/uploads'
 
 #머신러닝을 통해 나온 텍스트파일을 malicious, benign으로 분리해서 pdf 이름 저장
 #malicious, benign 리스트 리턴
 def output():
-    list_file = open('a.txt', 'r').read().split('\n')
+    list_file = open('output/out.txt', 'r').read().split('\n')
+    list_file=list_file[:-1]
     malicious_list=[]
     benign_list=[]
     for file in list_file:
@@ -29,6 +30,7 @@ def uploadfile():
 
 @app.route('/uploader', methods=['POST'])
 def uploader_file():
+    os.systen("sh output/code.sh")
     if request.method=='POST':
         upload = request.files.getlist("file[]")
         for f in upload:
